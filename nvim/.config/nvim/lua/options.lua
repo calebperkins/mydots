b02150f111d3a4f1cd805eef7f14e6e7f03acb1d
Caleb Perkins <calebperkins@users.noreply.github.com>
2024-11-25 22:49:24 -0800

    Redo nvim config according to kickstart-modular

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,20 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode (useful for resizing selections)
+vim.opt.mouse = 'a'
+
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--vim.opt.showmode = false
